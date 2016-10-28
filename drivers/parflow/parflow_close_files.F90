447fe19532bc12ba6e9968535278bcd09f671fa8
Ethan Coon <ecoon@lanl.gov>
2016-10-27 21:28:09 -0600

    first cut at drivers for parflow

---
+++ drivers/parflow/parflow_close_files.F90
@@ -0,0 +1,21 @@
+subroutine parflow_close_files()
+  implicit none
+
+  ! close(166)
+  ! close(199)
+  close(1995)
+  close(1996) 
+  close(1997)
+  close(1998)
+  close(2000)
+  close(2001)
+  close(2002)
+  close(2003)
+  close(2004)
+  close(2005)
+  close(2006)
+  close(2007)
+  close(2008)
+  close(2009)
+
+end subroutine parflow_close_files

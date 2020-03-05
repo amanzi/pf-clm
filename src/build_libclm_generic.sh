c68c8efcd8af50e3d06f935c9d84943eb502254f
jbeisman <jbeisman@lanl.gov>
2020-03-04 17:59:39 -0700

    complete CMake build system

---
+++ src/build_libclm_generic.sh
@@ -0,0 +1,11 @@
+
+ORIGIN_DIR=$PWD
+mkdir build ; cd build
+cmake .. \
+  -DCMAKE_INSTALL_PREFIX=$ORIGIN_DIR \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DUNSTRUCTURED_HOST=ON
+make -j 1 VERBOSE=1
+make install
+cd $ORIGIN_DIR
+

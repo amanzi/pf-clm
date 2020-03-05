177d39e200a2f5210e274e67af3ddc2bb17c69ab
jbeisman <jbeisman@lanl.gov>
2020-03-04 23:53:58 -0700

    moved CMakeLists.txt to top of source tree

--- build_libclm_generic.sh
+++ build_libclm_generic.sh
@@ -3,7 +3,7 @@ ORIGIN_DIR=$PWD
 mkdir build ; cd build
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$ORIGIN_DIR \
-  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_SHARED_LIBS=ON \
   -DUNSTRUCTURED_HOST=ON
 make -j 1 VERBOSE=1
 make install

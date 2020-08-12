6d0f39de3ec15cfad1acd6704f06fcced7b7248a
Ethan Coon <coonet@ornl.gov>
2020-08-11 21:12:17 -0400

    fixes clm inf and nan for newer compilers

--- src/utils/clm_infnan.F90
+++ src/utils/clm_infnan.F90
@@ -13,8 +13,17 @@ module clm_infnan
  ! real(r8), parameter :: inf = O'777600000000000000000' 
   !real(r8), parameter :: nan = O'777677777777777777777' 
 !#else 
-  real(r8), parameter :: inf = O'17740000000'
-  real(r8), parameter :: nan = O'17757777777'
+  integer :: int_infinity
+  integer :: int_nan
+  data int_infinity/O'17740000000'/
+  data int_nan/O'17757777777'/
+  real(r8) inf
+  real(r8) nan
+  EQUIVALENCE (inf,int_infinity)
+  EQUIVALENCE (nan,int_nan)
+
+!  real(r8), parameter :: inf = O'17740000000'
+!  real(r8), parameter :: nan = O'17757777777'
   real(r8), parameter :: invalid = -9999.0
 !#endif
 
